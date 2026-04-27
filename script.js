a5dd9a940656d1c1c28bc91f24fcd457684cb2ef
Wesley <159152636+1esley@users.noreply.github.com>
2026-04-27 17:13:36 +0000

    feat: pagina home

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ document.addEventListener("DOMContentLoaded", function () {
     mensagem.textContent = "Olá! O JavaScript está funcionando corretamente.";
   });
 });
+
+// abrir modal
+const botao = document.getElementById("botao");
+const modal = document.getElementById("modal");
+
+
+botao.addEventListener("click", () => {
+    modal.classList.add("ativo");
+});
+
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.classList.remove("ativo");
+    }
+});
